659a6c69f045c0075c93017db21e66f3d68ad140
Alessandro Somigli <alessandro.somigli.04@gmail.com>
2023-03-25 17:34:00 +0100

    added right api

--- src/main.tsx
+++ src/main.tsx
@@ -6,25 +6,31 @@ import { Apps } from './pages/Apps'
 import { Movies } from './pages/Movies'
 import { Books } from './pages/Books'
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
 
-    <Navbar />
-    
-    <Routes>
+        <Navbar />
 
-        <Route path="/" element={<Navigate to="/store" replace />} />
-        <Route path="/store" element={<Navigate to="/store/games" replace />} />
-        
-        <Route path="/store/games" element={<Games />} />
-        <Route path="/store/apps" element={<Apps />} />
-        <Route path="/store/movies" element={<Movies />} />
-        <Route path="/store/books" element={<Books />} />
+        <Routes>
 
-    </Routes>
-    </BrowserRouter>
+            <Route path="/" element={<Navigate to="/store" replace />} />
+            <Route path="/store" element={<Navigate to="/store/games" replace />} />
+            
+            <Route path="/store/games" element={<Games />} />
+            <Route path="/store/apps" element={<Apps />} />
+            <Route path="/store/movies" element={<Movies />} />
+            <Route path="/store/books" element={<Books />} />
+
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 )
--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,11 +1,22 @@
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from 'react-query'
+import axios from 'axios';
+
 type GamesProps = {
     children?: React.ReactNode
 }
 
 const Games = (props: GamesProps) => {
+    const queryClient = useQueryClient()
+
+    const gamesQuery = useQuery('games', () =>
+        axios.get("https://fair-teal-alligator-boot.cyclic.app/games")
+            .then(res => res.data) )
 
     return (
-        <>games!</>
+        <>
+            games! <br />
+            {console.log(gamesQuery.data)} <br />
+        </>
     )
 }
 
